593871195df180f846d63758e4d3e4349e74ba9b
Vincent Desforges <desforges.vincent@gmail.com>
2018-02-21 16:12:14 +0000

    Complete the yelp challen ge

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # GET "restaurants"
+
+  # GET "restaurants/new"
+  # POST "restaurants"
+
+  # GET "restaurants/38"
+
+  # GET "restaurants/38/reviews/new"
+  # POST "restaurants/38/reviews"
+  resources :restaurants, only: [:index, :new, :create, :show] do
+
+    resources :reviews, only: [:new, :create] do
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,75 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+Review.destroy_all
+
+puts 'Creating restaurants and reviews...'
+
+5.times do
+  restaurants_attributes = {
+    name:           Faker::Hobbit.character,
+    address:        Faker::Hobbit.location,
+    phone_number:   Faker::PhoneNumber.cell_phone,
+    category:       ["chinese", "italian", "japanese", "french", "belgian"].sample
+  }
+  restaurant = Restaurant.new(restaurants_attributes)
+  restaurant.save
+
+  10.times do
+    review_attributes = {
+      content:        Faker::Beer.name,
+      restaurant_id:  restaurant.id,
+      rating:         [0, 1, 2, 3, 4, 5].sample
+    }
+    review = Review.new(review_attributes)
+    review.save
+  end
+end
+
+puts 'Finished!'
+
+# puts 'Creating restaurants...'
+# restaurants_attributes = [
+#   {
+#     name:           'Dishoom',
+#     address:        '7 Boundary St, London E2 7JE',
+#     phone_number:   '+447123456789',
+#     category:       'chinese'
+#   },
+#   {
+#     name:           'Pizza East',
+#     address:        '56A Shoreditch High St, London E1 6PQ',
+#     phone_number:   '+447111111111',
+#     category:       'italian'
+#   },
+#   {
+#     name:           'Sushi Shop',
+#     address:        'London street, London',
+#     phone_number:   '+447222222222',
+#     category:       'japanese'
+#   }
+# ]
+# Restaurant.create!(restaurants_attributes)
+
+# dishoom = Restaurant.where(name: "Dishoom").first
+# # p dishoom.id
+
+# puts 'Creating reviews...'
+# reviews_attributes = [
+#   {
+#     content:        'Really great place, friendly staff',
+#     restaurant_id:  dishoom.id,
+#     rating:         5
+#   },
+#   {
+#     content:        'Horrific, almost died when I ate the soup...',
+#     restaurant_id:  dishoom.id,
+#     rating:         1
+#   }
+# ]
+
+# Review.create!(reviews_attributes)
+# puts 'Finished!'
